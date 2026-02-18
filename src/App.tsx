cf7790d4263b45b9451c1bea313e8091eae3f009
lr2larminat <lr2larminat@gmail.com>
2026-02-18 18:47:32 +0100

    Modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Individu, Famille } from './types';
 import { useState } from 'react';
+import { Individu, Famille } from './types';
 import HomePage from './HomePage';
 import ImportPage from './import/ImportPage';
 import ParsePage from './import/ParsePage';
@@ -9,8 +9,6 @@ export type Page = 'home' | 'import' | 'parse';
 function App() {
   const [currentPage, setCurrentPage] = useState<Page>('home');
   const [file, setFile] = useState<File | null>(null);
-  const [individuals, setIndividuals] = useState<Individu[]>([]);
-  const [families, setFamilies] = useState<Famille[]>([]);
   
   return (
     <>
